af08686281accedbc54eaa21470bd4c778623545
greatuber123 <greatuber123@gmail.com>
2024-04-05 09:25:54 -0500

    Installing dependencies to start working with views

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import ExampleComponent from './components/ExampleComponent.vue';
+app.component('example-component', ExampleComponent);
+
+app.mount('#app');
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,35 +1,21 @@
-import _ from 'lodash';
-window._ = _;
+import _ from 'lodash'
+import * as bootstrap from 'bootstrap'
+import axios from 'axios'
+import swal from 'sweetalert2'
+import jquery from 'jquery'
+import 'datatables.net-bs5'
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+window.$ = jquery
+window._ = _
+window.swal = swal
+window.axios = axios
+window.bootstrap = bootstrap
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest' // ajax
 
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+const csrf_token = document.head.querySelector('meta[name="csrf-token"]')
+if (csrf_token) {
+	window.csrf_token = csrf_token.content
+	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.csrf_token
+} else {
+	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token')
+}
